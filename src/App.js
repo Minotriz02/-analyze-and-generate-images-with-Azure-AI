f59f0b7c78b180c172aeb82e465a5a07e454e223
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-11-20 20:59:11 -0500

    input added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    if (inputValue.startsWith('http')) {
+      setImageUrl(inputValue);
+      setPrompt('');
+    } else {
+      setPrompt(inputValue);
+      setImageUrl('');
+    }
+    console.log('Image analysis triggered');
+  };
+
+  const handleImageGeneration = () => {
+    if (inputValue.startsWith('http')) {
+      setImageUrl(inputValue);
+      setPrompt('');
+    } else {
+      setPrompt(inputValue);
+      setImageUrl('');
+    }
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <div>
+      <h1>Image Analysis and Generation</h1>
+      <label>
+        Image URL / Prompt:
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </label>
+      <br />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
