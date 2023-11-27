73848c3b5a50cdae5143e6af6c6bd4e256f92ccc
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-11-27 16:33:33 -0500

    validation for analysis

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import {
   isConfiguredGeneration,
 } from "./azure-image-generation";
 import "./App.css";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Modal, Spinner } from "react-bootstrap";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [response, setResponse] = useState(null); // Initialize response as null
   const [responseGenerated, setResponseGenerated] = useState(null); // Initialize response as null
+  const [analysisValidation, setAnalysisValidation] = useState(true);
+  const [showAnalysis, setShowAnalysis] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -25,31 +27,56 @@ function App() {
   };
 
   const handleImageAnalysis = async () => {
-    if (inputValue.startsWith("http")) {
-      setIsLoading(true);
+    if (inputValue === "" || !inputValue.startsWith("http")) {
+      setAnalysisValidation(false);
+    } else if (inputValue.startsWith("http")) {
+      setAnalysisValidation(true);
+      setShowAnalysis(true);
       const data = await analyzeImage(inputValue);
       setResponse(data);
-      setIsLoading(false);
+      setShowAnalysis(false);
     }
   };
 
   return isConfiguredGeneration() && isConfiguredAnalysis() ? (
     <div>
+      <Modal show={showAnalysis} backdrop="static" keyboard={false} centered>
+        <Modal.Body className="d-flex align-items-center ">
+          <Spinner animation="border" variant="primary" className="me-2" />
+          Your image are being analyzed, please wait a moment...
+        </Modal.Body>
+      </Modal>
       <h1>VisionForge</h1>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Image URL / Prompt:</Form.Label>
           <Form.Control
+            required
             type="text"
             placeholder="url or image to generate"
             value={inputValue}
             onChange={handleInputChange}
+            className={`${!analysisValidation ? "border-danger " : ""}}`}
           />
+          {!analysisValidation && (
+            <label className="text-danger fs-6 mt-1">
+              Please a valid URL to a image
+            </label>
+          )}
         </Form.Group>
+        <Button
+          onClick={handleImageAnalysis}
+          accordion-collapse
+          className="me-2"
+        >
+          Analyze Image
+        </Button>
+        <button class="glow-on-hover" onClick={handleImageGeneration}>
+          Generate Image
+        </button>
       </Form>
       <br />
-      <Button onClick={handleImageAnalysis} className="me-2">Analyze Image</Button>
-      <Button onClick={handleImageGeneration}>Generate Image</Button>
+
       {isLoading && <p>Loading...</p>}
       {response && (
         <div>
@@ -62,7 +89,7 @@ function App() {
           <h2>Generated Image</h2>
           <img
             src={responseGenerated.data[0].url}
-            alt="Generated image"
+            alt="Generated"
             className="img-generated"
           />
           <pre style={{ maxWidth: "500px" }}>
